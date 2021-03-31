69d428a5582dffc73fc61b29f7807cd7fe34747b
Filipe Calegario <fcac@cin.ufpe.br>
2021-03-31 13:26:20 -0300

    Removing old .numbers file

--- library_generator.py
+++ library_generator.py
@@ -6,7 +6,7 @@ workbook = load_workbook(filename="PROBATIO_I2C_ADDRESS_SPACE.xlsx")
 file_h = 'probatio_defs.h'
 output_file = open(file_h, 'w')
 
-file_generate_array = 'probatio_array.cpp'
+file_generate_array = 'probatio_array.txt'
 output_file_array = open(file_generate_array, 'w')
 
 
@@ -19,6 +19,8 @@ output_file_array.write('/*\nProbatio populating array:\n*/\n\n')
 
 blocks_names = str()
 
+cc = 10
+
 for row in sheet.iter_rows(values_only=True):
     block_address = row[1]
     block_name = row[3]
@@ -29,9 +31,10 @@ for row in sheet.iter_rows(values_only=True):
         output_file.write(f'#define SIZE_BLOCK_{block_name} {block_data_size}\n\n')
         count_blocks = count_blocks + 1
         count_blocks_size = count_blocks_size + int(block_data_size)
-        output_file_array.write(f'Block {block_name.lower()}(BLOCK_{block_name}, SIZE_BLOCK_{block_name}, 0, 0, \"{block_name.lower()}\");\n')
+        output_file_array.write(f'Block {block_name.lower()}(BLOCK_{block_name}, SIZE_BLOCK_{block_name}, 0, {cc}, \"{block_name.lower()}\");\n')
         blocks_names = ', '.join([blocks_names, f'&{block_name.lower()}'])
         #blocks_names = blocks_names + f', &{block_name.lower()}'
+        cc = cc + int(block_data_size)
 
 buffer_size = count_blocks + count_blocks_size + 2
 
@@ -43,4 +46,4 @@ output_file_array.write(f'Block* blocks[QUANTITY_BLOCKS] = {{{blocks_names}}};')
 output_file.close()
 output_file_array.close()
 
-os.system('cp -f ' + file_h + ' /Users/fcac/Dropbox/Arduino/libraries/probatio/')
+# os.system('cp -f ' + file_h + ' /Users/fcac/Dropbox/Arduino/libraries/probatio/')
